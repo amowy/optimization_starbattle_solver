611773d56ed8449b7ba5cba9cc836704bb2e01c8
amowy <gyorgy.matyi@gmail.com>
2025-05-31 13:20:46 +0300

    Complete bounty: aaaaaaaaaa

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-//valami valmi test meg tobb teszt even more test even more test menj mar pls plsplspls plswork this time pls i beg no more deadlocks
+//valami valmi test meg tobb teszt even more test even more test menj mar pls plsplspls plswork this time pls i beg no more deadlocks aaaaaaaaaaaaaaaaaaaaaaaaaa
 use rand::{Error, Rng};
 use std::collections::{HashMap, HashSet};
 use std::time::{Duration, Instant};
